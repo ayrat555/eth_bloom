01b4ce53e15c25ebe6d227122838705ef90dc625
Ayrat Badykov <ayratin555@gmail.com>
2017-10-28 20:36:35 +0300

    initial commit

---
+++ lib/eth_bloom.ex
@@ -0,0 +1,18 @@
+defmodule EthBloom do
+  @moduledoc """
+  Documentation for EthBloom.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> EthBloom.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/eth_bloom_test.exs
@@ -0,0 +1,8 @@
+defmodule EthBloomTest do
+  use ExUnit.Case
+  doctest EthBloom
+
+  test "greets the world" do
+    assert EthBloom.hello() == :world
+  end
+end
