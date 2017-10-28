170b7dac82573df1e06332617c00208958ce226c
Ayrat Badykov <ayratin555@gmail.com>
2017-10-28 21:24:16 +0300

    sketch

--- mix.exs
+++ mix.exs
@@ -11,18 +11,15 @@ defmodule EthBloom.Mixfile do
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:keccakf1600, "~> 2.0.0", hex: :keccakf1600_orig}
     ]
   end
 end
